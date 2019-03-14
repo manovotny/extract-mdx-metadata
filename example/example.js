554d0d7ce27ef69ffad9b23cf55272ee47f8e647
Michael Novotny <manovotny@gmail.com>
2019-03-13 21:31:39 -0500

    WIP it, WIP it good

--- example/example.js
+++ example/example.js
@@ -3,43 +3,34 @@ const fs = require('fs');
 const {parse} = require('@babel/parser');
 const generate = require('@babel/generator').default;
 const traverse = require('@babel/traverse').default;
-const mdxAstToMdxHast = require('@mdx-js/mdx/mdx-ast-to-mdx-hast');
 const mdx = require('@mdx-js/mdx');
 const visit = require('unist-util-visit');
 
-(() => {
+(async () => {
     const path = 'example/example.mdx';
     const contents = fs.readFileSync(path);
+
     let meta;
 
-    const asdf = mdx.sync(contents, {
-        hastPlugins: [
-            mdxAstToMdxHast,
+    await mdx(contents, {
+        mdPlugins: [
             () => (tree) => {
-                for (let i = 0; i < tree.children.length; i++) {
-                    const {type, value} = tree.children[i];
+                visit(tree, 'export', (node) => {
+                    const ast = parse(node.value, {plugins: ['jsx'], sourceType: 'module'});
 
-                    if (type === 'export') {
-                        const ast = parse(value, {sourceType: 'module'});
-
-                        traverse(ast, {
-                            VariableDeclarator: (path) => {
-                                if (path.node.id.name === 'meta') {
-                                    meta = eval(`module.exports = ${generate(path.node.init).code}`);
-                                    return;
-                                }
+                    traverse(ast, {
+                        VariableDeclarator: (path) => {
+                            if (path.node.id.name === 'meta') {
+                                meta = eval(`module.exports = ${generate(path.node.init).code}`);
+                                return;
                             }
-                        });
-                    }
-
-                    if (meta) {
-                        break;
-                    }
-                }
+                        }
+                    });
+                });
             }
         ]
     });
 
-    console.log('asdf', asdf);
     console.log('meta', meta);
+    console.log('typeof meta', typeof meta);
 })();
