9c723277bd481bbdf725592ea32e4c3f06ac97ca
Michael Novotny <manovotny@gmail.com>
2021-09-03 14:07:12 -0500

    Got jest working!

---
+++ tests/index.test.js
@@ -0,0 +1,48 @@
+import {join} from 'path';
+import {readFileSync} from 'fs';
+
+import {wrap} from 'jest-snapshot-serializer-raw';
+
+import extractMdxMeta from '../index.js';
+
+const getFixture = (name) => {
+    const directory = new URL('.', import.meta.url).pathname;
+    const path = join(directory, 'fixtures', `${name}.mdx`);
+
+    return path;
+};
+
+test('should extract meta', async () => {
+    const path = getFixture('meta');
+
+    const meta = await extractMdxMeta(path);
+
+    expect(wrap(meta)).toMatchSnapshot();
+});
+
+test('should use default return value when no meta exists', async () => {
+    const path = getFixture('no-meta');
+
+    const meta = await extractMdxMeta(path);
+
+    expect(wrap(meta)).toMatchSnapshot();
+});
+
+test('should use custom return value when no meta exists', async () => {
+    const options = {
+        defaultReturnValue: undefined,
+    };
+    const path = getFixture('no-meta');
+
+    const meta = await extractMdxMeta(path, options);
+
+    expect(wrap(meta)).toMatchSnapshot();
+});
+
+test('should noop for any other export not named "meta"', async () => {
+    const path = getFixture('other');
+
+    const meta = await extractMdxMeta(path);
+
+    expect(wrap(meta)).toMatchSnapshot();
+});
